110ae9da728e1a10a17a60dfc962040fd8583390
comp500 <comp500@users.noreply.github.com>
2022-08-22 00:55:10 +0100

    Fix build and server

--- src/main/java/link/infra/beamforming/Beamforming.java
+++ src/main/java/link/infra/beamforming/Beamforming.java
@@ -5,14 +5,12 @@ import link.infra.beamforming.blocks.PrismBlockEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +35,5 @@ public class Beamforming implements ModInitializer {
 		Registry.register(Registry.ITEM, id("prism"), new BlockItem(PRISM, new QuiltItemSettings().group(ItemGroup.TRANSPORTATION)));
 
 		Registry.register(Registry.BLOCK_ENTITY_TYPE, id("prism"), PRISM_BE);
-
-		BlockRenderLayerMap.put(RenderLayer.getTranslucent(), PRISM);
 	}
 }
--- src/main/java/link/infra/beamforming/BeamformingClient.java
+++ src/main/java/link/infra/beamforming/BeamformingClient.java
@@ -3,10 +3,14 @@ package link.infra.beamforming;
 import link.infra.beamforming.blocks.PrismBlockEntityRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap;
+
+import static link.infra.beamforming.Beamforming.PRISM;
 
 public class BeamformingClient implements ClientModInitializer {
 	private static String ID;
@@ -25,5 +29,7 @@ public class BeamformingClient implements ClientModInitializer {
 			registry.register(id("block/beam"));
 			registry.register(id("block/beam_end"));
 		});
+
+		BlockRenderLayerMap.put(RenderLayer.getTranslucent(), PRISM);
 	}
 }
